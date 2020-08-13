a8d843503c3cb793f1cdacb52d0f72d078822cfe
Sindre Haavaldsen <sindre.h@outlook.com>
2020-08-13 21:41:18 +0200

    Connected React to API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [text, setText] = useState("")
+
+  const hello = () => {
+    fetch('http://localhost:8080/api/hello')
+    .then(response => response.text())
+    .then(message => setText(message))
+  }
+
+  hello()
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {text}
         </p>
         <a
           className="App-link"
--- src/main/java/com/sindreha/quizapp/HelloController.java
+++ src/main/java/com/sindreha/quizapp/HelloController.java
@@ -1,5 +1,6 @@
 package com.sindreha.quizapp;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import java.util.Date;
 
 @RestController
 public class HelloController {
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/api/hello")
     public String hello() {
         return "Hello, the time at the server is now " + new Date() + "\n";
