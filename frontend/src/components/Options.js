7d80ed4bcb0cd6a1d1d5324c2f8b67f905df7120
Sindre Haavaldsen <sindre.h@outlook.com>
2020-08-14 16:04:52 +0200

    Log answer method

--- frontend/src/components/Options.js
+++ frontend/src/components/Options.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 
 export default function Options(props) {
 
+    const print = (msg) => console.log(msg)
+    print()
+
     return (
         <>
         {   
-            props.options.map((option) => (
+            props.options.map((option, i) => (
                 <div 
+                    onClick={() => {
+                        props.handler(i)
+                    }}
                     key={option.id} 
-                    className="option">
+                    className={`option
+                    ${props.myAnswer === i ? "selected" : null}`}>
                     <p>{option}</p>
                 </div>
             ))
--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -10,7 +10,10 @@ export default class Quiz extends Component {
         super()
         this.state = {
             currentQuestion: 0,
-            options: []
+            options: [],
+            myAnswer: null,
+            answers: [],
+            disableButton: true
         }
     }
 
@@ -32,37 +35,63 @@ export default class Quiz extends Component {
     loadQuizData = () => {
         this.setState(() => {
           return {
+            questionId: quizData[this.state.currentQuestion].id,
             question: quizData[this.state.currentQuestion].question,
             answer: quizData[this.state.currentQuestion].answer,
             options: quizData[this.state.currentQuestion].options
           };
         });
-      };
+    }
+
+    logAnswer = () => {
+        const answer = 
+            {
+                question: this.state.questionId,
+                answer: this.state.myAnswer
+            }
+
+        this.setState(prevState => ({
+            answers: [...prevState.answers, answer]
+        }))
+    }
 
     nextQuestionHandler = () => {
-    //const { myAnswer, answer, score } = this.state;
+        this.logAnswer()
+        this.setState({myAnswer: null, disableButton: true})
+        //const { myAnswer, answer, score } = this.state;
 
-    /* if (myAnswer === answer) {
+        /* if (myAnswer === answer) {
+            this.setState({
+            score: score + 1
+            });
+        } */
+
         this.setState({
-        score: score + 1
+            currentQuestion: this.state.currentQuestion+1
         });
-    } */
-
-    this.setState({
-        currentQuestion: this.state.currentQuestion+1
-    });
-    };
+    }
+    
+    selectionHandler = answer => {
+        this.setState({ myAnswer: answer, disableButton: false });
+    }
 
     render() {
 
-        const {question, options, currentQuestion} = this.state;
+        const {
+            question, 
+            options, 
+            currentQuestion, 
+            myAnswer, 
+            disableButton} 
+        = this.state;
 
         return (
             <div className="quiz-container">
                 <Question question={question}/>
                 <ProgressIndicator quizData={quizData} currentQuestion={currentQuestion}/>
-                <Options options={options} />
+                <Options options={options} handler={this.selectionHandler} myAnswer={myAnswer}/>
                 <button 
+                    disabled={disableButton}
                     onClick={() => this.nextQuestionHandler()}>
                     Neste spørsmål
                 </button>
